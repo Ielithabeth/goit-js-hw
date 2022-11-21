d9b101652163b66d52d7227751745bf35de62376
Ielithabeth <lisa.pltrtskaya@gmail.com>
2022-11-21 12:41:53 +0200

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const vimeoPlayer = document.querySelector('#vimeo-player');
+const player = new Player(vimeoPlayer);
+
+player.on("timeupdate", throttle(savePlayerTime,1000));
+player.on('loaded', updatePlayerTime);
+
+function savePlayerTime({seconds}) {
+    localStorage.setItem("videoplayer-current-time",seconds);
+};
+
+function updatePlayerTime() {
+    if (localStorage.getItem("videoplayer-current-time")){
+    player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+}};
